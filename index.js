845bc5d5139d7d3ef685d4f986042299ccc42952
Tristan Straub <tristan.straub@adslot.com>
2013-11-29 10:34:15 +1100

    Flexible filenames

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 (function() {
   fs = require('fs');
-  exports.__LOG__ = function(id) {
-    var logFileName;
-    logFileName = '/tmp/log.json';
-    console.log('log', id);
-    return function(value) {
-      var collection, e;
-      try {
-        collection = JSON.parse(fs.readFileSync(logFileName));
-      } catch (_error) {
-        e = _error;
-        collection = [];
-      }
-      collection.push({
-        file: __filename,
-        id: id,
-        value: value
-      });
-      return fs.writeFileSync(logFileName, JSON.stringify(collection));
+  exports.logger = function(logFilename, loggedFilename) {
+    return function(id, value) {
+      var logFileName;
+      return function(value) {
+        var collection, e;
+        try {
+          collection = JSON.parse(fs.readFileSync(logFileName));
+        } catch (_error) {
+          e = _error;
+          collection = [];
+        }
+        collection.push({
+          file: loggedFilename,
+          id: id,
+          value: value
+        });
+        return fs.writeFileSync(logFilename, JSON.stringify(collection));
+      };
     };
   };
 
