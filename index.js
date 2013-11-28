fb07857a76cf2e9462a4ea6b84fce4a5873f8097
Tristan Straub <tristan.straub@adslot.com>
2013-11-29 10:24:30 +1100

    coffeescript? not here..

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-exports.__LOG__ = (id) ->
-  logFileName = '/tmp/log.json'
+(function() {
+  exports.__LOG__ = function(id) {
+    var logFileName;
+    logFileName = '/tmp/log.json';
+    console.log('log', id);
+    return function(value) {
+      var collection, e;
+      try {
+        collection = JSON.parse(fs.readFileSync(logFileName));
+      } catch (_error) {
+        e = _error;
+        collection = [];
+      }
+      collection.push({
+        file: __filename,
+        id: id,
+        value: value
+      });
+      return fs.writeFileSync(logFileName, JSON.stringify(collection));
+    };
+  };
 
-  console.log 'log', id
-  (value) ->
-    console.log 'logging'
-    try
-      collection = JSON.parse fs.readFileSync logFileName
-    catch e
-      collection = []
-
-    collection.push {
-      file: __filename
-      id: id
-      value: value
-    }
-
-    console.log 'logging', collection
-    fs.writeFileSync logFileName, JSON.stringify collection
+}).call(this);
